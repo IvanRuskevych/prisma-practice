1247e1e292f0feea55d0e955359dc091e3eb80fd
IvanRuskevych <ivan.ruskevych@gmail.com>
2024-07-29 00:51:57 +0300

    create globalErrorHandler & routesErrorHandler & class ApiExceptions

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,1 +1,3 @@
+export * from './globalErrorHandler';
+export * from './routesErrorHandler';
 export * from './isExistMiddleware';
--- src/middleware/isExistMiddleware.ts
+++ src/middleware/isExistMiddleware.ts
@@ -2,23 +2,25 @@ import { NextFunction, Request, Response } from 'express';
 
 import { prismaClient } from '../prisma/prismaClient';
 import { getModelDelegate } from '../utils';
+import { ApiExceptions } from '../exceptions';
 
 export const isExistMiddleware = (model: keyof typeof prismaClient, id: string) => {
   return async (req: Request, res: Response, next: NextFunction) => {
     const reqId = req.body[id] || req.params[id];
 
     if (!reqId) {
-      return console.log('reqId is missing');
+      return next(ApiExceptions.BadRequest('ID is required'));
     }
 
     try {
       const record = await getModelDelegate(model).findUnique({
-        where: { id: id },
+        where: { id: reqId },
       });
 
       if (!record) {
-        return console.log('Record not found');
+        return next(ApiExceptions.NotFound(`Record with the given id not found`));
       }
+
       req.body.record = record;
 
       next();
